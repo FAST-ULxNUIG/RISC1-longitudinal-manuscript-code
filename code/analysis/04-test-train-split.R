b4adb62c76c044a577bde6bd401e656fc2d6974a
edwardgunning <edward.gunning@ul.ie>
2023-06-30 16:13:38 +0100

    add code for table to test train split script.

--- code/analysis/04-test-train-split.R
+++ code/analysis/04-test-train-split.R
@@ -5,9 +5,10 @@
 # ------------------------------------------------------------------------#
 
 # Packages and functions needed -------------------------------------------
-library(data.table) # CRAN v1.14.0
-library(fda)        # CRAN v5.5.1
-
+library(data.table)   # CRAN v1.14.0
+library(fda)          # CRAN v5.5.1
+library(stringr)      # CRAN v1.4.0
+library(modelsummary) # CRAN v1.4.1
 
 # -------------------------------------------------------------------------
 outputs_path <- here::here("outputs")
@@ -30,9 +31,46 @@ bspl80 <- fda::create.bspline.basis(
   nbasis = 80,
   norder = 4)
 
-# -------------------------------------------------------------------------
 
+
+                      # Create table 1 of characteristics: -------------------------------------
 subset_info_dt <- subset_coef_hip
+table1_dt <- subset_info_dt[, .(
+  ris = retrospective_injury_status[1],
+  speed = self_selected_speed_kmph[1],
+  sex = sex[1],
+  age = age[1],
+  weight = weight_kg[1],
+  height = height_cm[1]),
+  by = subject_id]
+table1_dt[, ris := factor(ris, levels = c("never_injured",
+                                          "injured_greater_than_2_yr",
+                                          "injured_1_to_2_yr", 
+                                          "injured_less_than_1_yr"),
+                          labels = c("Never Injured",
+                                     "Injured $>2$ yr. ago",
+                                     "Injured $1-2$ yr. ago",
+                                     "Injured $<1$ yr. ago"))]
+table1_dt[, sex := factor(sex, levels = c("male", "female"), labels = c("Male", "Female"))]
+setnames(table1_dt,
+         old = c("ris", "speed", "sex", "age", "weight", "height"),
+         new = c("Retrospective Injury Status", "Speed (kmph)", "Sex", "Age (years)", "Weight (kg)", "Height (cm)"))
+table1 <- datasummary_balance(~ 1, data = table1_dt[, - c("subject_id")],
+                              output = "latex", escape = FALSE)
+table1 <- stringr::str_replace(table1,
+                               pattern = "& N & Pct.\\\\\\\\\n",
+                               replacement = "& N & Pct.\\\\\\\\\n\\\\midrule\n")
+table1 <- stringr::str_replace(table1, pattern = "N & Pct.", 
+                               replacement = "\\\\textbf{N} & $\\\\mathbf{\\\\pmb{(\\\\%)}}$")
+table1 <- stringr::str_replace(table1, pattern = "Mean & Std. Dev.", 
+                               replacement = "\\\\textbf{Mean} & \\\\textbf{Std. Dev.}")
+table1 <- stringr::str_replace(table1, pattern = "\\\\end\\{tabular\\}\n\\\\end\\{table\\}",
+                               replacement = "\\\\end\\{tabular\\}\\\n\\\\caption\\{Summary characteristics of the dataset used in the analysis.\\}\n\\\\label\\{tab:tab1.\\}\n\\\\end\\{table\\}")
+writeLines(text = table1, con = file.path(outputs_path, "tables", "table_1.tex"))
+
+
+
+# Scale variables: --------------------------------------------------------
 subset_info_dt[, `:=`(
   height_cm_cent = scale(height_cm, center = TRUE, scale = FALSE),
   weight_kg_cent = scale(weight_kg, center = TRUE, scale = FALSE),
@@ -67,9 +105,20 @@ test_strides <- covariates_dt[, .(test_strides = sample(unique(trial_id), size =
 train_inds <- which(!(covariates_dt$trial_id %in% test_strides))
 test_inds <- which(covariates_dt$trial_id %in% test_strides)
 
+stopifnot(length(unique(covariates_dt$trial_id))  == length(test_inds) + length(train_inds))
+length(unique(covariates_dt$trial_id)) 
+# 47150
+length(test_inds)
+#  5680
+length(train_inds)
+#  41470
+
+
 mfd_obj_train <- mfd_obj[train_inds,, ]
 covariates_dt_train <- covariates_dt[train_inds]
 stopifnot(ncol(mfd_obj_train$coefs) == covariates_dt_train[, .N])
+
+
 
 mfd_obj_test <- mfd_obj[test_inds,, ]
 covariates_dt_test <- covariates_dt[test_inds]
@@ -77,6 +126,7 @@ stopifnot(ncol(mfd_obj_test$coefs) == covariates_dt_test[, .N])
 
 par(mfrow = c(1, 3))
 plot(mfd_obj_test[1:100,,])
+
 
 test_train_objects_list <- list(
   mfd_obj_train = mfd_obj_train,
